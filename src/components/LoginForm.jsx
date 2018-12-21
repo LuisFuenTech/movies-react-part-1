2bc8410bda7e541efec852f42d2d5f22cb67c9de
Luis Fuentes <luisfuau10@gmail.com>
2018-12-21 00:19:11 -0500

    Login form updated

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   handleSubmit = e => {
     e.preventDefault();
 
@@ -8,17 +18,34 @@ class LoginForm extends Component {
   };
 
   render() {
+    const { username, password } = this.state.account;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={username}
+              onChange={this.handleChange}
+              autoFocus
+              id="username"
+              name="username"
+              type="text"
+              className="form-control"
+            />
           </div>
+
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="password"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
