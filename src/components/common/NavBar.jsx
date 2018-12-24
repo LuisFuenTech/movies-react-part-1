116e115abd41f6b971215da20c35bdc1d6dd4a0c
Luis Fuentes <luisfuau10@gmail.com>
2018-12-23 23:11:41 -0500

    Added: register form, new movie form and seacrh box

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -19,7 +19,7 @@ const NavBar = () => {
         <span className="navbar-toggler-icon" />
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
+        <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <NavLink className="nav-link" to="/movies">
               Movies
@@ -38,6 +38,11 @@ const NavBar = () => {
           <li className="nav-item">
             <NavLink className="nav-link" to="/login">
               Login
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/register">
+              Register
             </NavLink>
           </li>
         </ul>
